0184f10ebeabe802f7cf6b78cdf73e1737cb2f4f
chenlai <chenlai@devvm2090.vll0.facebook.com>
2021-04-28 15:07:10 -0700

    [PyTorch][Edge] Add api to get bytecode version from runtime
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/56948
    
    Add api to get runtime bytecode version
    
    ## Test
    Both `caffe2/test/cpp/jit/test_lite_interpreter.cpp` and `caffe2/test/mobile/test_bytecode.py` pass
    ghstack-source-id: 127674754
    
    Differential Revision: [D27987811](https://our.internmc.facebook.com/intern/diff/D27987811/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D27987811/)!

---
+++ torch/csrc/jit/mobile/runtime_bytecode_version.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <memory>
+
+namespace torch {
+namespace jit {
+namespace mobile {
+
+TORCH_API int64_t _get_runtime_bytecode_version();
+
+} // namespace mobile
+} // namespace jit
+} // namespace torch
